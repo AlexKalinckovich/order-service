895e50a903d22cd0943c38e3625498155458a7bc
Alex <aleckseykalinckovich@gmail.com>
2025-07-15 11:05:43 +0300

    -add initial-schema -add necessary models -add docker-compose.yml -add Dockerfile

---
+++ src/main/java/com/example/orderservice/model/Order.java
@@ -0,0 +1,38 @@
+package com.example.orderservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    @Enumerated
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private LocalDate orderDate;
+}
---
+++ src/main/java/com/example/orderservice/model/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.example.orderservice.model;
+
+public enum OrderStatus {
+}
